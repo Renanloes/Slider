b52d5714f23e067c3cf72df5f54f73cbb2b1d5e6
Renan Loes <renanloes8@gmail.com>
2024-01-23 18:33:34 -0300

    adicao do inicio do js

---
+++ index.js
@@ -0,0 +1,9 @@
+let slides = document.querySelectorAll('.slide');
+let setaEsquerda = document.getElementsByClassName('seta-esquerda');
+let setaDireita = document.getElementsByClassName('seta-direita');
+
+setaEsquerda.addEventListeners("click", () => {
+    
+})
+
+
